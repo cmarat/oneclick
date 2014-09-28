d1a98be59fce2037fc5cc801ae7d5b2f70f120d4
Marat Charlaganov <charlaganov@gmail.com>
2014-09-28 18:13:31 +0200

    Fix installation of standalone templates

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@
 from setuptools import setup, find_packages
 
 requirements = open('requirements.txt').read().splitlines()
-setup(name='CommitDemo',
-      version='0.1',
-      package_data={'linkitup': ['plugins.yaml', 'templates/*.html',
-                  'templates/*.query', 'templates/*.js', 'static/js/*.js',
-                  'static/css/*.css', 'static/css/*.png', 'static/img/*.png',
-                  'static/js/vendor/*.js', 'migrations/*.*',
-                  'migrations/versions/*']},
-      packages=find_packages(),
-      scripts=['manage.py'],
-      install_requires=requirements)
+setup(
+    name='CommitDemo',
+    version='0.1',
+    package_data={
+        'linkitup': [
+            'plugins.yaml', 'templates/*.html',
+            'templates/*.query', 'templates/*.js', 'static/js/*.js',
+            'static/css/*.css', 'static/css/*.png', 'static/img/*.png',
+            'static/js/vendor/*.js', 'migrations/*.*',
+            'migrations/versions/*','standalone/templates/*.*'
+        ]
+    },
+    packages=find_packages(),
+    scripts=['manage.py'],
+    install_requires=requirements)
